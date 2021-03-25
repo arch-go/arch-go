eb3c547801809f6371b2701ee0499c966e6a6d16
Francisco Daines <francisco.daines@walmart.com>
2021-03-25 10:51:52 -0300

    Removes fmt.Printf and includes error in return value

--- impl/utils/get_main_package.go
+++ impl/utils/get_main_package.go
@@ -13,13 +13,8 @@ func GetMainPackage() (string, error) {
 	if _, err := os.Stat(goModFile); err == nil {
 		content, _ := ioutil.ReadFile(goModFile)
 		modulePath := modfile.ModulePath(content)
-		fmt.Printf("Module: %s\n", modulePath)
 		return modulePath, nil
-	} else if os.IsNotExist(err) {
-		fmt.Printf("Could not load %s file.\n", goModFile)
-		return "", err
 	} else {
-		fmt.Printf("Could not load %s file.\n", goModFile)
-		return "", err
+		return "", fmt.Errorf("Could not load %s file. %s\n", goModFile, err.Error())
 	}
 }
