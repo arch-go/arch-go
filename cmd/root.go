b82bec76c08637ce68b9202afe9db0d8e5460ec8
Francisco Daines <francisco.daines@walmart.com>
2021-03-25 16:44:28 -0300

    Refactor in root command

--- cmd/root.go
+++ cmd/root.go
@@ -7,23 +7,25 @@ import (
 	"os"
 )
 
-func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		os.Exit(1)
-	}
-}
-
 var rootCmd = &cobra.Command{
 	Use:     "arch-go",
 	Version: common.Version,
 	Short:   "Architecture checks for Go",
 	Long: `Architecture checks for Go:
 * Dependency checks`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Not Implemented.\n")
-	},
+	Run: runCommand,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
 
 func init() {
 	rootCmd.PersistentFlags().BoolVarP(&common.Verbose, "verbose", "v", false, "Verbose Output")
+}
+
+func runCommand(cmd *cobra.Command, args []string) {
+	fmt.Printf("Not Implemented.\n")
 }
