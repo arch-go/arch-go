a76dae714c30ecb2e336a84ad2e4296a4fb5d3d8
Francisco Daines <francisco.daines@walmart.com>
2022-02-21 13:32:27 -0300

    Sonarcloud integration

---
+++ internal/config/load_config.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+func LoadConfig(configPath string) (*Config, error) {
+	config := &Config{}
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	d := yaml.NewDecoder(file)
+	if err := d.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
