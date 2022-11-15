cb7e947f74a88e276caf1454c6baadac0168f7a2
Francisco Daines <fdaines@gmail.com>
2022-11-15 16:31:11 -0300

    Fixes resources path for HTML report

--- internal/report/html/helpers.go
+++ internal/report/html/helpers.go
@@ -3,6 +3,7 @@ package html
 import (
 	"flag"
 	"fmt"
+	"github.com/fdaines/arch-go/internal/common"
 	"os"
 )
 
@@ -23,8 +24,7 @@ func copyAssets() {
 	if _, err := os.Stat(".arch-go/"); os.IsNotExist(err) {
 		os.Mkdir(".arch-go", 0755)
 	}
-	css, _ := os.ReadFile("./internal/report/html/templates/report.css")
-	cssByteArray := []byte(css)
+	cssByteArray, _ := os.ReadFile(archGoModulePath() + "/internal/report/html/templates/report.css")
 	os.WriteFile(".arch-go/report.css", cssByteArray, 0644)
 }
 
@@ -32,7 +32,11 @@ func resolveTemplateRelativePath(template string) string {
 	if isTestRun() {
 		return "./templates/" + template
 	}
-	return "./internal/report/html/templates/" + template
+	return archGoModulePath() + "/internal/report/html/templates/" + template
+}
+
+func archGoModulePath() string {
+	return os.Getenv("GOPATH") + "/pkg/mod/github.com/fdaines/arch-go@v" + common.Version
 }
 
 func isTestRun() bool {
