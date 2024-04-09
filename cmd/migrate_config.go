3391d93653e28b58d233cb14b845267bc25de2f0
Francisco Daines <fdaines@gmail.com>
2024-04-09 16:59:01 -0400

    refactor: Migrates migrate-config command to new structure

---
+++ cmd/migrate_config.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"fmt"
+	migrate_config "github.com/fdaines/arch-go/pkg/commands/migrate-config"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+func NewMigrateConfigCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "migrate-config",
+		Short: "Migrate architecture configuration (arch-go.yml) to current schema",
+		Run:   migrateConfig,
+	}
+}
+
+func init() {
+	migrateCmd := NewMigrateConfigCommand()
+	rootCmd.AddCommand(migrateCmd)
+}
+
+func migrateConfig(cmd *cobra.Command, args []string) {
+	migrate_config.NewCommand(cmd.OutOrStdout(), getWorkingDirectory()).Run()
+}
+
+func getWorkingDirectory() string {
+	wd, err := os.Getwd()
+	if err != nil {
+		fmt.Printf("Error: %+v\n", err)
+		os.Exit(1)
+	}
+	return wd
+}
---
+++ cmd/migrate_config_test.go
@@ -0,0 +1,118 @@
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/spf13/viper"
+
+	monkey "github.com/agiledragon/gomonkey/v2"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/fdaines/arch-go/old/config"
+)
+
+func TestMigrateConfigCommand(t *testing.T) {
+	viper.AddConfigPath("../test/")
+
+	t.Run("when arch-go.yaml contains current schema but has no rules", func(t *testing.T) {
+		var exitCode int
+		cmd := NewMigrateConfigCommand()
+		patch := monkey.ApplyFuncReturn(config.LoadConfig, &config.Config{}, nil)
+		defer patch.Reset()
+		patchExit := monkey.ApplyFunc(os.Exit, func(c int) { exitCode = c })
+		defer patchExit.Reset()
+
+		b := bytes.NewBufferString("")
+		cmd.SetOut(b)
+		cmd.Execute()
+		out, _ := io.ReadAll(b)
+
+		expected := `Invalid Configuration: configuration file should have at least one rule
+`
+		assert.Equal(t, expected, string(out))
+		assert.Equal(t, 1, exitCode)
+	})
+
+	t.Run("when arch-go.yaml contains current schema and has rules", func(t *testing.T) {
+		var exitCode int
+		cmd := NewMigrateConfigCommand()
+		currentConfiguration := config.Config{
+			FunctionsRules: []*config.FunctionsRule{
+				{
+					Package:  "foobar",
+					MaxLines: 10,
+				},
+			},
+		}
+		patch := monkey.ApplyFuncReturn(config.LoadConfig, &currentConfiguration, nil)
+		defer patch.Reset()
+		patchExit := monkey.ApplyFunc(os.Exit, func(c int) { exitCode = c })
+		defer patchExit.Reset()
+
+		b := bytes.NewBufferString("")
+		cmd.SetOut(b)
+		cmd.Execute()
+		out, _ := io.ReadAll(b)
+
+		expected := `File is already compatible with version 1
+`
+		assert.Equal(t, expected, string(out))
+		assert.Equal(t, 0, exitCode)
+	})
+
+	t.Run("when arch-go.yaml contains old schema", func(t *testing.T) {
+		var exitCode int
+		cmd := NewMigrateConfigCommand()
+		patch := monkey.ApplyFuncReturn(config.LoadConfig, nil, fmt.Errorf("not found"))
+		defer patch.Reset()
+		patchOldSchema := monkey.ApplyFuncReturn(config.LoadDeprecatedConfig, &config.DeprecatedConfig{}, nil)
+		defer patchOldSchema.Reset()
+		patchExit := monkey.ApplyFunc(os.Exit, func(c int) { exitCode = c })
+		defer patchExit.Reset()
+
+		b := bytes.NewBufferString("")
+		cmd.SetOut(b)
+		cmd.Execute()
+		out, _ := io.ReadAll(b)
+
+		expected := `Migrating deprecated configuration to current schema.
+Deprecated configuration backup at: old-arch-go.yml
+Configuration saved at: arch-go.yml
+`
+		_, err1 := os.Stat("old-arch-go.yml")
+		os.Remove("old-arch-go.yml")
+		_, err2 := os.Stat("arch-go.yml")
+		os.Remove("arch-go.yml")
+
+		assert.Nil(t, err1, string(out))
+		assert.Nil(t, err2, string(out))
+		assert.Equal(t, expected, string(out))
+		assert.Equal(t, 0, exitCode)
+
+	})
+
+	t.Run("when arch-go.yaml cannot be loaded", func(t *testing.T) {
+		var exitCode int
+		cmd := NewMigrateConfigCommand()
+		patch := monkey.ApplyFuncReturn(config.LoadConfig, nil, fmt.Errorf("not found"))
+		defer patch.Reset()
+		patchOldSchema := monkey.ApplyFuncReturn(config.LoadDeprecatedConfig, nil, fmt.Errorf("not loaded"))
+		defer patchOldSchema.Reset()
+		patchExit := monkey.ApplyFunc(os.Exit, func(c int) { exitCode = c })
+		defer patchExit.Reset()
+
+		b := bytes.NewBufferString("")
+		cmd.SetOut(b)
+		cmd.Execute()
+		out, _ := io.ReadAll(b)
+
+		expected := `Error: not loaded
+`
+		assert.Equal(t, expected, string(out))
+		assert.Equal(t, 1, exitCode)
+	})
+}
