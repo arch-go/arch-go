765b1ae59951abf351082bf59d83d9dae388e65c
Yuriy Kazanov <y.kazanov@tinkoff.ru>
2025-11-11 17:36:23 +0300

    feat: correct matching packages without suffix

--- internal/utils/text/regexp.go
+++ internal/utils/text/regexp.go
@@ -6,6 +6,9 @@ import (
 )
 
 func PreparePackageRegexp(p string) string {
+	if !strings.Contains(p, "*") {
+		return p
+	}
 	str := p
 
 	re := regexp.MustCompile(`(\w)+\*`)
@@ -34,16 +37,16 @@ func PreparePackageRegexp(p string) string {
 
 	if strings.HasSuffix(str, ".**") {
 		idx := strings.LastIndex(str, ".**")
-		str = str[:idx] + strings.Replace(str[idx:], ".**", "(/[\\w-\\.]+)*$", 1)
+		str = str[:idx] + strings.Replace(str[idx:], ".**", "(/[\\w-\\.]+)*", 1)
 	}
 
 	if strings.HasSuffix(str, ".*") {
 		idx := strings.LastIndex(str, ".*")
-		str = str[:idx] + strings.Replace(str[idx:], ".*", "(/[\\w-\\.]+){0,1}$", 1)
+		str = str[:idx] + strings.Replace(str[idx:], ".*", "(/[\\w-\\.]+){0,1}", 1)
 	}
 
 	str = strings.ReplaceAll(str, ".**.", "(/[\\w-\\.]+/)+")
 	str = strings.ReplaceAll(str, ".*.", "/[\\w-\\.]+/")
 
-	return str
+	return str + "$"
 }
